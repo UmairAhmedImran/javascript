891249ecc2bcf390e91bc3e27630b8291abfc37a
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2023-08-13 23:50:23 +0500

    Creating a function file

---
+++ function.js
@@ -0,0 +1,14 @@
+function countdown() {
+    console.log("5")
+    console.log("4")
+    console.log("3")
+    console.log("2")
+    console.log("1")
+}
+
+
+countdown()
+
+// instead of typing aall code two times it is better to create a function one time and call it two times
+
+countdown()
